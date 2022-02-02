6ed6755b4df4ee1965d25eca8f9bcf1b66bac649
bboygg <bboygg94@gmail.com>
2022-02-02 17:40:23 +0900

    finish racket

--- src/main/java/Racket.java
+++ src/main/java/Racket.java
@@ -7,6 +7,7 @@ public class Racket {
 
     /*****RACKET SETTING*****/
     public int racketColor = p.color(0);
+    public int racketBounceRate = 20;
     public int racketWidth = 100;
     public int racketHeight = 10;
 
@@ -19,14 +20,22 @@ public class Racket {
         p.rect(p.mouseX, p.mouseY, racketWidth, racketHeight, 5);
     }
 
-    //Actually I don't understand how this method works... Let's check tmr lol
+    //racketBounce() method is make sure that the racket adn the ball collides.
+    //There's three of nested if statements, first one is verify to lined up X coordinate between Ball and Racket
+    //Second one is verify Y coordinate has lined up or not.
+    //dist() - calculate between two points
+
+
     public void racketBounce(){
         float overhead = p.mouseY - p.pmouseY;
         if((b.ballX + (b.ballSize/2) > p.mouseX - (racketWidth/2)) && (b.ballX - (b.ballSize/2) < p.mouseX + (racketWidth / 2))){
-            if(p.dist(b.ballX, b.ballY, b.ballX,p.mouseY) <= (b.ballSize / 2) + p.abs(overhead)){
+            //if(p.dist(b.ballX, b.ballY, b.ballX, p.mouseY) <= (b.ballSize / 2) + p.abs(overhead)){
+            if( p.abs(p.mouseY - b.ballY) <= (b.ballSize / 2) + p.abs(overhead)){
                 b.bounceBottom(p.mouseY);
-                b.ballSpeedHorizon = (b.ballX - p.mouseX) / 10;
+                b.ballSpeedHorizon = (b.ballX - p.mouseX) / 10; //change horizontal direction according to the collipse position
 
+                // overhead negative means that the position of the racket upper than previous position.
+                // So, we will speed up to the ball in this case.
                 if(overhead < 0){
                     b.ballY += (overhead / 2);
                     b.ballSpeedVert += (overhead/2);
--- src/main/java/Wall.java
+++ src/main/java/Wall.java
@@ -1,2 +1,50 @@
+import processing.core.PApplet;
+import java.util.ArrayList;
+
 public class Wall {
+
+    PApplet p;
+    Ball b;
+    Racket r;
+
+    /*****WALL SETTINGS*****/
+    public int wallWidth = 80;
+    public int wallColor = p.color(44, 62, 80);
+    public int wallSpeed = 5;
+    public int wallInterval = 100;
+    public float lastAddTime = 0;
+    public int minGapHeight = 200;
+    public int maxGapHeight = 300;
+
+    //This arrayList will  stores data of the gaps between the walls
+    //Actuals walls are drawn accordingly
+    ArrayList<int[]> walls = new ArrayList<int[]>();
+
+    /*****WALL BEHAVIORS*****/
+    public void wallAdder(){
+        if(p.millis() - lastAddTime > wallInterval){
+            int randHeight = p.round(p.random(minGapHeight, maxGapHeight));
+            int randY = p.round(p.random(0, p.height - randHeight));
+            int[] randWall = {p.width, randY, wallWidth, randHeight, 0};
+            walls.add(randWall);
+            lastAddTime = p.millis();
+        }
+    }
+    public void wallDrawer(int index){
+        int[] wall = walls.get(index);
+
+
+    }
+
+    public void wallHandler(){
+        for(int i = 0; i < walls.size(); i++){
+            wallRemover(i);
+            wallMover(i);
+            wallDrawer(i);
+            wallCollision(i);
+        }
+    }
+
+
+
 }
